36a8517b7ea2d39277374045552eb6620342cfee
zsiyed <69491969+zsiyed@users.noreply.github.com>
2022-01-20 13:50:49 -0800

    lab 3 breaking test

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,13 +10,18 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
+        System.out.println(currentIndex);
+
         while(currentIndex < markdown.length()) {
+
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            System.out.println(currentIndex);
+
         }
         return toReturn;
     }
